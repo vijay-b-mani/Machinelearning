94aeaed2451bb9266141a898629ee04b9dfd260e
Vijay B Mani <42358545+vijay-b-mani@users.noreply.github.com>
2018-08-13 17:15:06 -0400

    Create vectoralgebra.py

---
+++ vectoralgebra.py
@@ -0,0 +1,7 @@
+## Sample code for ##
+
+##Vector addtion, multiplication, subtraction, Scalar multiplication, Unit vertor, Normalize, angle between two vectors
+
+#####################
+
+import os
